9f363df44186d6e69be0ec290865b61312ecf1e8
vaishnavi59265 <65214256+vaishnavi59265@users.noreply.github.com>
2021-07-21 14:31:35 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,57 @@
+<?php include('nav.php')?>
+<html>
+      <head>
+             
+             <link rel="stylesheet" type="text/css" href="home.css">
+       </head>
+
+       <body>
+	        <header> 
+	           <div class="header">
+	               <div class="inner-header">
+	   
+	                       <div class="logo">
+	                         Indian Bank
+	                       </div>
+						   
+						   
+                           <div class="header-link">
+	                       <a href="transfer.php" class="active">view customer</a>
+	                       </div>
+
+						   
+                           <div class="header-link">
+							<a href="transactionhistory.php" class="active">Transission History</a>
+							</div>
+		   
+							<div class="header-link">
+	                       <a href="createuser.php" class="active">CreateUser</a>
+	                       </div>
+		          
+		   
+	                  </div>
+	           </div>
+	       </header>
+			   <div class="content">
+			        <div class="banner-image">
+					      <div class="inner-banner-image">
+					           <div class="banner-content">
+							        <img src="images.png" width="120" height="120">
+									 <p><h2>Welcome to</h2>
+									    <h1>INDIAN BANK</h1></p>
+									 
+							   </div>
+					      </div>
+					</div>  
+			   </div>
+			   
+			  
+			   
+			    <footer>
+			        <div class="container">
+			            <p>Copyright ©️2021 Made by VAISHNAVI CHAVAN</p>
+						<p></p>
+			        </div>
+			    </footer>		
+      </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,25 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sparks Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="home.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+</head>
+
+
+<body>
+
+
+
+    </div>
+  </nav>
